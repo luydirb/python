699abd2909d11139900277f96de98745f084623f
luydirb <luydi.bertolazzo@gmail.com>
2023-06-18 17:11:24 -0300

    commit

--- import matplotlib.pyplot as plt; plt.py
+++ import matplotlib.pyplot as plt; plt.py
@@ -4,7 +4,7 @@ import numpy as np
 musicas = ('Lib Provisória', 'Sentadão', 'Combatchy', 'Surtada', 'Cheirosa')
 indice = np.arange(len(musicas))
 acessos = [1068254,866216,849895,763652,758198]
-plt.bar(indice, acessos)
+plt.bar(indice, acessos, color=["cyan","red","gray","yellow",(0.8,0.5,1.0,1.0)], edgecolor='blue' )
 plt.xticks(indice, musicas)
 plt.ylabel('Acessos')
 plt.title('Ranking do Spotify 31.dez.2019')
